c44543308ccd79114d8a6b5531799147c2a1478c
Osho2110 <osho1719@gmail.com>
2024-05-06 17:22:12 +0530

    Added undeliverable location exception.

--- blinkitInit.py
+++ blinkitInit.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 def slow_type(element, text, delay=0.05):
         for character in text:
                 ActionChains(driver).move_to_element(element).click().send_keys(character).perform()
@@ -18,3 +21,11 @@ reAddressBar = driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[1]/head
 slow_type(reAddressBar, pinCode[-3:])
 addressSuggestion = driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[1]/header/div[2]/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div[1]')
 addressSuggestion.click()
+try:
+    
+    addressSuggestion = WebDriverWait(driver, 2).until(  
+        EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div[1]/header/div[2]/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div[1]')))
+    addressSuggestion.click()
+
+except TimeoutException:
+    print("Sorry for the inconvenience, Blinkit doesn't deliver at your location.")
