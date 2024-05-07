8d1d4cf2276de7a199fcae5649d30341b35d444b
Osho2110 <osho1719@gmail.com>
2024-05-07 21:35:54 +0530

    Made changes to display product information.

--- blinkitInit.py
+++ blinkitInit.py
@@ -12,21 +12,26 @@ def typeSim(element, text, delay=0.05):
                 ActionChains(driver).move_to_element(element).click().send_keys(character).perform()
                 time.sleep(delay)
 
-#⚠️REMOVE DEFAULT VALUE FOR QUERY⚠️
-def blinksearch(query="Aloo Bhujia"):
+
+def blinkSearch(driver):
         driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div[1]/header/div[2]/a/div[2]/div').click()
         searchbar=driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div[1]/header/div[2]/div/input')
         searchbar.clear()
-        searchbar.send_keys(query)
-        pickfirst()
+        searchbar.send_keys(productName)
+        BlinkfirstResult()
         
-        
-        
+           
+def BlinkfirstResult():
+        blinkR1Title = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[3]/div/div/div[2]/div[1]/div/div/div/div[2]/div[2]/a[1]/div/div[2]/div[2]/div[1]/div[1]').text
+        blinkR1Quantity = driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[3]/div/div/div[2]/div[1]/div/div/div/div[2]/div[2]/a[1]/div/div[2]/div[2]/div[1]/div[2]/div').click()
+        BlinkR1Q1 = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div/div/div[2]/div[1]/div/div[1]/div[2]').text
+        BlinkR1Q1prc = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div/div/div[2]/div[1]/div/div[2]/div/div').text
+        BlinkR1OP = f"Product Info : {BlinkR1Q1} {blinkR1Title} @ {BlinkR1Q1prc}"
+        print(BlinkR1OP)
 
-def pickfirst():
-        print("First item: ",driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[3]/div/div/div[2]/div[1]/div/div/div/div[2]/div[2]/a[1]/div/div[2]/div[2]/div[1]/div[1]').text)
-        
+
 pinCode= input("Enter Pincode : ")
+productName = input("Enter Product Name : ")
 driver = webdriver.Firefox()
 driver.get("https://blinkit.com")
 driver.implicitly_wait(2)
@@ -48,5 +53,5 @@ try:
     print("Sorry for the inconvenience, Blinkit doesn't deliver at your location.")
     driver.quit()
 except (TimeoutException, NoSuchElementException):
-                print("delivery available at your pincode")
-                blinksearch()
+                print("Delivery available at your location, fetching relevant results.")
+                blinkSearch(driver)
