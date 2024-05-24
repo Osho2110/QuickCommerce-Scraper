939aa8f220492e0b6afa856e76f8671b4df7aebc
Alwin Showi <69798846+alw8n@users.noreply.github.com>
2024-05-25 00:48:58 +0530

    added AJAX POST method for search term and minor visual bug fix

--- static/searchbar.js
+++ static/searchbar.js
@@ -1,9 +1,12 @@
+
+
 const items = [
     { name: "Aloo Bhujia" },
     { name: "Coke" },
     { name: "Pepsi" },
     { name: "Lays" },
-    { name: "Head and Shoulders" }
+    { name: "Head and Shoulders" },
+    { name: "Haldiram's Aloo bhujia" }
 ];
 
 const options = {
@@ -14,6 +17,9 @@ const fuse = new Fuse(items, options);
 
 const searchInput = document.getElementById('search-input');
 const results = document.getElementById('results');
+const searchbutton = document.getElementById('search-submit');
+
+
 
 searchInput.addEventListener('input', () => {
     const searchQuery = searchInput.value;
@@ -31,7 +37,24 @@ searchInput.addEventListener('input', () => {
             });
             results.appendChild(listItem);
         });
-    } else {
-        results.textContent = 'No matching results found.';
+    } else {// modified content because of display issues
+        const listItem = document.createElement('ul'); 
+        listItem.textContent = "No results found";
+        results.appendChild(listItem);
     }
+});
+
+searchbutton.addEventListener('click',() => {
+    searchterm=searchInput.value;
+    $.ajax({ 
+        url: '/searchbar', 
+        type: 'POST', 
+        data: { 'searchterm': searchterm }, 
+        success: function(response) { 
+            document.getElementById('output').innerHTML = response; 
+        }, 
+        error: function(error) { 
+            console.log(error); 
+        } 
+    }); 
 });
