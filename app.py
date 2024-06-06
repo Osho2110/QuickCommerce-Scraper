03ca33b8076367b2e92f8409103a149e98962868
Alwin Showi <alwinshowi@gmail.com>
2024-06-06 14:32:34 +0530

    Implemented threading to improve time efficiency and refactored code

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template, request 
-import blinkitInit as bk
-
+import blinkit as bk
+import bigbasket as bb
+import threading
 
 app = Flask(__name__,template_folder="templates") 
 
@@ -9,20 +10,28 @@ def hello():
 	return render_template('index.html') 
 
 @app.route('/pincode_post', methods=['POST']) 
-def pincode_post(): 
+def pincode_post():
+	global thread_a, thread_b
 	data = request.form.get('pin') 
-	p=str(data) 
-	bk.pinin(p)
+	p=str(data)
+	thread_a = threading.Thread(target=bk.BlinkCheckAvailability,args=(p,))
+	thread_b = threading.Thread(target=bb.BigCheckAvailability,args=(p,))
+	thread_a.start()
+	thread_b.start()
 	result = f"your data is {p}" 
 	return result 
 
 @app.route('/searchbar', methods=['POST'])
 def searchbar():
+	global thread_a, thread_b, thread_c, thread_d
 	search = request.form.get('searchterm') 
-	s=str(search) 
-	bk.blinkSearch(s)
+	s=str(search)
+	thread_c = threading.Thread(target=bk.blinkSearch,args=(s,))
+	thread_d = threading.Thread(target=bb.bigSearch,args=(s,))
+	thread_c.start()
+	thread_d.start()
 	result = f"your data is {s}" 
 	return result 
 
 if __name__ == '__main__': 
-	app.run(debug=True) 
+	app.run(debug=True)
